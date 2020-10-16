77b24d80305d8fde70e776b53c5f8705f529f118
TamirMegged <tamir.m.nx@gmail.com>
2020-10-16 18:55:51 +0300

    hvi

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const cors = require('cors');
-const db = require('./db');
-const port = 1000;
-
+const path = require('path');
 const app = express();
+const cors = require('cors');
+const port = 1000;
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
 
 app.use(express.json());
 app.use(cors());
@@ -11,8 +12,8 @@ app.use("/users", require('./routes/users'));
 app.use("/vacations", require('./routes/vacations'));
 app.use("/likes", require('./routes/likes'));
 
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
 
 app.listen(port, () => console.log(`up and running on ${port}`));
