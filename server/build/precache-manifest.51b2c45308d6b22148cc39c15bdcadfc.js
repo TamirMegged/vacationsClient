f2de2068b2262c43d71ce8866bb723f82b01ee49
TamirMegged <tamir.m.nx@gmail.com>
2020-10-16 19:20:23 +0300

    DSFASG

---
+++ server/build/precache-manifest.51b2c45308d6b22148cc39c15bdcadfc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c94e326e9a3f0a495b8dfad40b86b29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c80a2a9a35d2e9f09c3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "380549e83cbf1160abb1",
+    "url": "/static/js/2.7fec9376.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/static/js/2.7fec9376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c80a2a9a35d2e9f09c3",
+    "url": "/static/js/main.4df21d66.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "c2982aa31a75bb4c4f123672f0fa4847",
+    "url": "/static/media/logo.c2982aa3.png"
+  },
+  {
+    "revision": "4ce37a351178ec7e4e740a8e98f6241d",
+    "url": "/static/media/world.4ce37a35.png"
+  }
+]);
--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const path = require('path');
+// const favicon = require('express-favicon');
 const app = express();
 const cors = require('cors');
-const port = 1000;
-const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
-
+const port = process.env.PORT || 1000;
+// const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.json());
 app.use(cors());
 app.use("/users", require('./routes/users'));
 app.use("/vacations", require('./routes/vacations'));
 app.use("/likes", require('./routes/likes'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`up and running on ${port}`));
